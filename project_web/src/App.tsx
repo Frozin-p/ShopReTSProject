1b061514c22a3a4dc42c73e86b99911ae8a101f8
Frozin-p <nikitin278@bk.ru>
2024-05-21 00:14:35 +0300

    Добавлено больше карточек товаров, а также сделан слайдер

--- project_web/src/App.tsx
+++ project_web/src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import { Product } from './types/Product';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
 
 const App: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -30,6 +34,27 @@ const App: React.FC = () => {
         description: 'Air Jordan 5 «Olive», которые изначально вышли в 2006 году, впервые вернутся весной 2024 года. Эта пара была частью коллекции Jordan Brand Lifestyle (LS) и вышла вместе с соответствующей одеждой. Эту расцветку многие считают одной из лучшей в линейке AJ5 за все время.',
         imageUrl: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/baaa5611-b560-4e94-a4e2-61b698c68432/air-jordan-5-retro-big-kids-shoes-Q7w8vv.png',
       },
+      {
+        id: 4,
+        name: 'J Balvin x Air Jordan 3 «Rio»',
+        price: 250,
+        description: 'Кроссовки будут представлены в контрастной комбинации цветов Black, Solar Flare и Total Crimson Abyss. Пора получит черную основу с градиентными деталями на заднике и мидсоли, а также классическими серыми элефант-вставками спереди и сзади.',
+        imageUrl: 'https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/0c411364-8e10-4bf7-ae0f-f99c300000dc/air-jordan-3-x-j-balvin-rio-fn0344-001-%E2%80%94-releasedatum.jpg',
+      },
+      {
+        id: 5,
+        name: 'Air Jordan 11 «Space Jam»',
+        price: 200,
+        description: 'Основа сникеров состоит из гладкой черной сетки, дополненной соответствующими черными брызговиками из лакированной кожи. В центре внимания – синие акценты Varsity Royal, заменяющие оригинальные фиолетовые детали Concord Purple, которые были в первом выпуске. Контрастная белая мидсоль на «ледяной» полупрозрачной аутсоли завершает дизайн модели.',
+        imageUrl: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c282fc56-171b-464b-9387-b39b19efe203/air-jordan-11-retro-low-black-varsity-royal-mens-shoes-MNm675.png',
+      },
+      {
+        id: 6,
+        name: 'Air Jordan 12 Retro "Taxi Flip"',
+        price: 200,
+        description: 'Верх обуви выполнен из мятой белой кожи с прошитыми линиями и ярким черным брызговиком с текстурой ящерицы. Яркий красный логотип Jumpman, элегантно вышитый на язычке, сочетается с накладкой из ТПУ в средней части стопы.',
+        imageUrl: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/fd7da2d3-50b4-4263-8450-ce88301a915f/air-jordan-12-retro-taxi-flip-mens-shoes-mmrpJt.png',
+      },
     ];
 
     setProducts(mockProducts);
@@ -39,8 +64,8 @@ const App: React.FC = () => {
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>Frozin`s</h1>
-       <ProductList products={products} />
+        <h1></h1>      
+        <ProductList products={products} />
       </div>
     </div>
   );
--- project_web/src/components/Navbar.tsx
+++ project_web/src/components/Navbar.tsx
@@ -5,12 +5,11 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a href="#" className="navbar-logo">Frozin`s</a>
+        <a href="/" className="navbar-logo">Frozin`s</a>
         <ul className="navbar-menu">
-          <li><a href="#" className="navbar-item">Главная</a></li>
-          <li><a href="#" className="navbar-item">Авторизация</a></li>
-          <li><a href="#" className="navbar-item">Контакты</a></li>
-          <li><a href="#" className="navbar-item">Личный кабинет</a></li>
+          <li><a href="/" className="navbar-item">Контакты</a></li>
+          <li><a href="/" className="navbar-item">Корзина</a></li>
+          <li><a href="/" className="navbar-item">Авторизация</a></li>
         </ul>
       </div>
     </nav>
