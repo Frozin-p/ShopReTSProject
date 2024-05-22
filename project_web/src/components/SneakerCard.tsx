438a61127a849684b67d040f6d8c159b649b7a25
Frozin-p <nikitin278@bk.ru>
2024-05-22 11:55:30 +0300

    Добавлена кнопка для покупки товаров

--- project_web/src/components/SneakerCard.tsx
+++ project_web/src/components/SneakerCard.tsx
@@ -12,8 +12,11 @@ const SneakerCard: React.FC<SneakerCardProps> = ({ name, description, price, ima
         <div className="sneaker-card">
             <img src={imageUrl} alt={name} />
             <h2>{name}</h2>
-            <p>{description}</p>
-            <p className='sneaker-price'>${price.toFixed(2)}</p>
+            <p className='sneaker-description'>{description}</p>
+            <div className="sneaker-card-footer">
+                <p className="sneaker-price">${price.toFixed(2)}</p>
+                <button className="add-to-cart-button">BUY</button>
+            </div>
         </div>
     );
 };
